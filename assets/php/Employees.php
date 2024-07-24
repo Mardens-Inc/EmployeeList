a9f36c4ad87349ed47ebcf8a3b55cd57ae1876b2
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-07-24 08:36:33 -0400

    Refactor Employee class to remove 'id' field
    The 'id' field has been removed from the Employee class and associated methods. The 'employee_id' field is now of type int and will be used as the unique identifier. This change simplifies the structure and reduces redundancy as both 'id' and 'employee_id' fields previously served as identifiers. Update on SQL queries have been made accordingly to reflect this change.

--- assets/php/Employees.php
+++ assets/php/Employees.php
@@ -6,15 +6,13 @@ use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
 class Employee
 {
-    public int $id;
-    public string $employee_id;
+    public int $employee_id;
     public string $first_name;
     public string $last_name;
     public string $location;
 
-    function __construct(int $id, string $employee_id, string $first_name, string $last_name, string $location)
+    function __construct(int $employee_id, string $first_name, string $last_name, string $location)
     {
-        $this->id = $id;
         $this->employee_id = $employee_id;
         $this->first_name = $first_name;
         $this->last_name = $last_name;
@@ -39,7 +37,7 @@ class Employees
         $first_name = $employee->first_name;
         $last_name = $employee->last_name;
         $location = $employee->location;
-        $stmt = $this->connection->prepare("INSERT INTO `employees` (employee_id, first_name, last_name, location) VALUES (?, ?, ?, ?)");
+        $stmt = $this->connection->prepare("INSERT INTO `employees` (id, first_name, last_name, location) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("isss", $employee_id, $first_name, $last_name, $location);
         return $stmt->execute();
     }
@@ -49,7 +47,7 @@ class Employees
         $result = $this->connection->query("SELECT * FROM `employees`");
         $employees = [];
         while ($row = $result->fetch_assoc()) {
-            $employee = new Employee($row["id"], $row["employee_id"], $row["first_name"], $row["last_name"], $row["location"]);
+            $employee = new Employee(intval($row["id"]), $row["first_name"], $row["last_name"], $row["location"]);
             $employees[] = $employee;
         }
         return $employees;
@@ -57,14 +55,14 @@ class Employees
 
     public function search(string $query): array
     {
-        $stmt = $this->connection->prepare("SELECT * FROM `employees` WHERE first_name LIKE ? OR last_name LIKE ? OR employee_id = ? OR CONCAT(LOWER(first_name), ' ', LOWER(last_name)) LIKE ? OR location LIKE ? ORDER BY location");
+        $stmt = $this->connection->prepare("SELECT * FROM `employees` WHERE first_name LIKE ? OR last_name LIKE ? OR id = ? OR CONCAT(LOWER(first_name), ' ', LOWER(last_name)) LIKE ? OR location LIKE ? ORDER BY location");
         $like = "%$query%";
         $stmt->bind_param("sssss", $like, $like, $query, $like, $like);
         $stmt->execute();
         $result = $stmt->get_result();
         $employees = [];
         while ($row = $result->fetch_assoc()) {
-            $employee = new Employee($row["id"], $row["employee_id"], $row["first_name"], $row["last_name"], $row["location"]);
+            $employee = new Employee(intval($row["id"]), $row["first_name"], $row["last_name"], $row["location"]);
             $employees[] = $employee;
         }
         return $employees;
@@ -79,7 +77,9 @@ class Employees
      */
     public function import(string $type, mixed $file): array
     {
-        $this->connection->query("TRUNCATE TABLE `employees`");
+        $this->connection->query("set foreign_key_checks = 0");
+        $this->connection->query("truncate table `employees`");
+        $this->connection->query("set foreign_key_checks = 1");
         $reader = new Xlsx();
         $spreadsheet = $reader->load($file["tmp_name"]);
         $worksheet = $spreadsheet->getActiveSheet();
@@ -87,7 +87,7 @@ class Employees
         array_shift($rows);
         $count = 0;
         foreach ($rows as $row) {
-            $employee = new Employee(0, $row[0], $row[1], $row[2], $row[3]);
+            $employee = new Employee($row[0], $row[1], $row[2], $row[3]);
             $this->add($employee);
             $count++;
         }
