bcbf48e2616e3204c3b5d38debe0c258c9f2ccba
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-24 16:30:56 -0400

    Update employee endpoints and add KRDP import endpoint
    Changed the URLs for existing employee endpoints for better clarity. Added a new endpoint for importing KRDP data from Excel files, which involves removing existing KRDP data and handling file operations.

--- src-actix/employees_endpoint.rs
+++ src-actix/employees_endpoint.rs
@@ -3,7 +3,7 @@ use actix_web::{get, post, web, Error, HttpResponse};
 use log::info;
 use std::collections::HashMap;
 
-#[get("/")]
+#[get("/employees")]
 pub async fn get_employees() -> Result<HttpResponse, Error> {
 	let employees = employees_database::get_employees().await.map_err(|err| {
 		actix_web::error::ErrorInternalServerError(format!(
@@ -15,7 +15,7 @@ pub async fn get_employees() -> Result<HttpResponse, Error> {
 	Ok(HttpResponse::Ok().json(employees))
 }
 
-#[get("/search")]
+#[get("/employees/search")]
 pub async fn search_employees(query: web::Query<HashMap<String, String>>) -> Result<HttpResponse, Error> {
 	// Extract the specific query parameter
 	let query_param = query.get("query").ok_or_else(|| {
@@ -33,13 +33,54 @@ pub async fn search_employees(query: web::Query<HashMap<String, String>>) -> Res
 	Ok(HttpResponse::Ok().json(employees))
 }
 
-#[post("/")]
+#[post("/import")]
 pub async fn import_from_excel_file(body: web::Bytes) -> Result<HttpResponse, Error> {
 	let path = "employees.xlsx";
 	let bytes = body.to_vec();
 	std::fs::write(path, bytes).map_err(|err| {
 		actix_web::error::ErrorInternalServerError(format!(
 			"Failed to write file: {}",
+			err
+		))
+	})?;
+
+	employees_database::remove_employees().await.map_err(|err| {
+		actix_web::error::ErrorInternalServerError(format!(
+			"Failed to remove employees: {}",
+			err
+		))
+	})?;
+
+	let count = employees_database::import_from_excel_file(path).await.map_err(|err| {
+		actix_web::error::ErrorInternalServerError(format!(
+			"Failed to import employees: {}",
+			err
+		))
+	})?;
+
+	std::fs::remove_file(path).map_err(|err| {
+		actix_web::error::ErrorInternalServerError(format!(
+			"Failed to remove file: {}",
+			err
+		))
+	})?;
+
+	Ok(HttpResponse::Ok().json(format!("Imported {} employees", count)))
+}
+#[post("/import/krdp")]
+pub async fn import_from_excel_file_krdp(body: web::Bytes) -> Result<HttpResponse, Error> {
+	let path = "krdp.xlsx";
+	let bytes = body.to_vec();
+	std::fs::write(path, bytes).map_err(|err| {
+		actix_web::error::ErrorInternalServerError(format!(
+			"Failed to write file: {}",
+			err
+		))
+	})?;
+
+	employees_database::remove_krdp().await.map_err(|err| {
+		actix_web::error::ErrorInternalServerError(format!(
+			"Failed to remove employees: {}",
 			err
 		))
 	})?;
