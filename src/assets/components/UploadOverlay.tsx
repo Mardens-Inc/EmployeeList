5e0c33427322504f5c1aa76838f6c37c621625d3
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-24 14:48:22 -0400

    Add UploadOverlay component for file drag-and-drop support
    Introduced a new UploadOverlay component to handle file drag-and-drop events over specific areas. The component visually indicates when files are being dragged over the Mardens Employees List and KRDP Employees List drop zones and logs the event on file drop.

---
+++ src/assets/components/UploadOverlay.tsx
@@ -0,0 +1,82 @@
+import {useEffect, useState} from "react";
+import $ from "jquery";
+import {cn} from "@nextui-org/react";
+
+export default function UploadOverlay()
+{
+    const [isDragging, setIsDragging] = useState(false);
+    const [isDraggingOverKRDP, setisDraggingOverKRDP] = useState(false);
+    const [isDraggingOverEmployees, setIsDraggingOverEmployees] = useState(false);
+
+    useEffect(() =>
+    {
+        $(document).on("dragover", (e) =>
+        {
+            e.preventDefault();
+            setIsDragging(true);
+        }).on("dragexit", () =>
+        {
+            setIsDragging(false);
+        });
+        $("#mardens-employees-drop-area").on("dragover", (e) =>
+        {
+            e.preventDefault();
+            setIsDraggingOverEmployees(true);
+        }).on("dragend", () =>
+        {
+            setIsDraggingOverEmployees(false);
+        }).on("drop", (e) =>
+        {
+            e.preventDefault();
+            setIsDraggingOverEmployees(false);
+            setIsDragging(false);
+            console.log(e);
+        });
+
+        $("#krdp-employees-list-drop-area").on("dragover", (e) =>
+        {
+            e.preventDefault();
+            setisDraggingOverKRDP(true);
+        }).on("dragexit", () =>
+        {
+            setisDraggingOverKRDP(false);
+        })
+            .on("drop", (e) =>
+            {
+                e.preventDefault();
+                setisDraggingOverKRDP(false);
+                setIsDragging(false);
+                console.log(e);
+            });
+
+    }, []);
+
+    return (
+        <div
+            className={
+                cn(
+                    "fixed inset-0 bg-black/50 z-50 backdrop-blur-sm flex flex-row items-center justify-end px-8 gap-8",
+                    "opacity-0 pointer-events-none data-[is-dragging=true]:opacity-100 data-[is-dragging=true]:pointer-events-auto transition-all"
+                )
+            }
+            data-is-dragging={isDragging}
+        >
+
+            <div
+                id={"mardens-employees-drop-area"}
+                className={"rounded-lg border-1 border-primary w-1/2 h-4/5 bg-primary/10 z-50 backdrop-blur-sm flex items-center justify-center opacity-30 data-[is-dragging-over=true]:opacity-100 transition-all"}
+                data-is-dragging-over={isDraggingOverEmployees}
+            >
+                <p className={"text-4xl text-primary"}>Mardens Employees List</p>
+            </div>
+            <div
+                id={"krdp-employees-list-drop-area"}
+                className={"rounded-lg border-1 border-primary w-1/2 h-4/5 bg-primary/10 z-50 backdrop-blur-sm flex items-center justify-center opacity-30 data-[is-dragging-over=true]:opacity-100 transition-all"}
+                data-is-dragging-over={isDraggingOverKRDP}
+            >
+                <p className={"text-4xl text-primary"}>KRDP Employees List</p>
+            </div>
+
+        </div>
+    );
+}
