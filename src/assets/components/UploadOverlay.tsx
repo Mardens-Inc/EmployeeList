fbc6eb311094d0cbde7edba29ea504347859b654
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-30 08:14:26 -0400

    Add upload progress indicators and success modal
    Introduce a loading spinner during the upload process and a modal to confirm a successful upload. Also, refactor file upload logic to trigger a modal displaying the number of uploaded employees.

--- src/assets/components/UploadOverlay.tsx
+++ src/assets/components/UploadOverlay.tsx
@@ -1,12 +1,16 @@
 import {useEffect, useState} from "react";
 import $ from "jquery";
-import {cn} from "@nextui-org/react";
+import {Button, cn, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Spinner} from "@nextui-org/react";
+import Employees from "../ts/employees.ts";
 
 export default function UploadOverlay()
 {
     const [isDragging, setIsDragging] = useState(false);
     const [isDraggingOverKRDP, setisDraggingOverKRDP] = useState(false);
     const [isDraggingOverEmployees, setIsDraggingOverEmployees] = useState(false);
+    const [showUploadModal, setShowUploadModal] = useState(false);
+    const [uploadCount, setUploadCount] = useState(0);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() =>
     {
@@ -42,9 +46,21 @@ export default function UploadOverlay()
         }).on("drop", (e) =>
         {
             e.preventDefault();
-            setIsDraggingOverEmployees(false);
-            setIsDragging(false);
-            console.log(e);
+            setLoading(true);
+
+            const file: File | undefined = e.originalEvent?.dataTransfer?.files[0];
+            if (file)
+            {
+                Employees.upload(file)
+                    .then(setUploadCount)
+                    .then(() => setShowUploadModal(true))
+                    .finally(() =>
+                    {
+                        setLoading(false);
+                        setIsDraggingOverEmployees(false);
+                        setIsDragging(false);
+                    });
+            }
         });
 
         krdpDropArea.on("dragover", (e) =>
@@ -58,40 +74,69 @@ export default function UploadOverlay()
             .on("drop", (e) =>
             {
                 e.preventDefault();
-                setisDraggingOverKRDP(false);
-                setIsDragging(false);
-                console.log(e);
+                setLoading(true);
+                const file: File | undefined = e.originalEvent?.dataTransfer?.files[0];
+                if (file)
+                {
+                    Employees.upload_krdp(file)
+                        .then(setUploadCount)
+                        .then(() => setShowUploadModal(true)).finally(() =>
+                    {
+                        setLoading(false);
+                        setisDraggingOverKRDP(false);
+                        setIsDragging(false);
+                    });
+                }
             });
 
 
     }, []);
 
     return (
-        <div
-            className={
-                cn(
-                    "fixed inset-0 bg-black/50 z-50 backdrop-blur-sm flex flex-row items-center justify-end px-8 gap-8",
-                    "opacity-0 pointer-events-none data-[is-dragging=true]:opacity-100 data-[is-dragging=true]:pointer-events-auto transition-all"
-                )
-            }
-            data-is-dragging={isDragging}
-        >
-
+        <>
+            <Modal isOpen={showUploadModal} onClose={() => setShowUploadModal(false)}>
+                <ModalContent>
+                    {onClose => (
+                        <>
+                            <ModalHeader>Upload Successful</ModalHeader>
+                            <ModalBody>
+                                <p>{uploadCount} Employees Uploaded</p>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button onClick={onClose}>Close</Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
             <div
-                id={"mardens-employees-drop-area"}
-                className={"rounded-lg border-1 border-primary w-1/2 h-4/5 bg-primary/10 z-50 backdrop-blur-sm flex items-center justify-center opacity-30 data-[is-dragging-over=true]:opacity-100 transition-all"}
-                data-is-dragging-over={isDraggingOverEmployees}
+                className={
+                    cn(
+                        "fixed inset-0 bg-black/50 z-50 backdrop-blur-sm flex flex-row items-center justify-end px-8 gap-8",
+                        "opacity-0 pointer-events-none data-[is-dragging=true]:opacity-100 data-[is-dragging=true]:pointer-events-auto transition-all"
+                    )
+                }
+                data-is-dragging={isDragging}
             >
-                <p className={"text-4xl text-primary"}>Mardens Employees List</p>
-            </div>
-            <div
-                id={"krdp-employees-list-drop-area"}
-                className={"rounded-lg border-1 border-primary w-1/2 h-4/5 bg-primary/10 z-50 backdrop-blur-sm flex items-center justify-center opacity-30 data-[is-dragging-over=true]:opacity-100 transition-all"}
-                data-is-dragging-over={isDraggingOverKRDP}
-            >
-                <p className={"text-4xl text-primary"}>KRDP Employees List</p>
-            </div>
 
-        </div>
+                <div
+                    id={"mardens-employees-drop-area"}
+                    className={"rounded-lg border-1 border-primary w-1/2 h-4/5 bg-primary/10 z-50 backdrop-blur-sm flex items-center justify-center opacity-30 data-[is-dragging-over=true]:opacity-100 transition-all"}
+                    data-is-dragging-over={isDraggingOverEmployees}
+                >
+                    {loading ? <Spinner size={"lg"}/> :
+                        <p className={"text-4xl text-primary"}>Mardens Employees List</p>}
+                </div>
+                <div
+                    id={"krdp-employees-list-drop-area"}
+                    className={"rounded-lg border-1 border-primary w-1/2 h-4/5 bg-primary/10 z-50 backdrop-blur-sm flex items-center justify-center opacity-30 data-[is-dragging-over=true]:opacity-100 transition-all"}
+                    data-is-dragging-over={isDraggingOverKRDP}
+                >
+                    {loading ? <Spinner size={"lg"}/> :
+                        <p className={"text-4xl text-primary"}>KRDP Employees List</p>}
+                </div>
+
+            </div>
+        </>
     );
 }
