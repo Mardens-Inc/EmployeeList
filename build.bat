a7cb3faa88d31743e38ded06d0815283c6b25cd6
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-31 16:21:34 -0400

    Add build.bat for automated build process
    This new script sets up an automated build process using specific network and SSH configurations. It targets the employees-database and runs "npm run build" for building the project. This helps streamline the build process and ensures consistency across environments.

---
+++ build.bat
@@ -0,0 +1,1 @@
+pwp -j 192.168.21.209 -u administrator -a C:\Users\drew.chase\.ssh\mss-sshkey_ossh.key -s employees-database --binary employees-database --build-command "npm run build" -p 22 --build
